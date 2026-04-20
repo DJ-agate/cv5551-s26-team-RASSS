7ced8598fc20df911407024476cb58f23ee9a22a
Andrew <joh15433@umn.edu>
2026-04-20 16:17:55 -0500

    fixed grasp collections and collected grasps

--- utils/collect_grasps_poses.py
+++ utils/collect_grasps_poses.py
@@ -1,46 +1,80 @@
 import pickle
+
+from utils.zed_camera import ZedCamera
+import cv2, time
+
+
 from transforms import get_transform_camera_robot, get_transform_cube, get_mug_from_april
-from scipy.spatial.transform import Rotation
+from scipy.spatial.transform import Rotation as R
 import numpy as np
 from xarm.wrapper import XArmAPI
 
 
-
 robot_ip = '192.168.1.172'
 
+def main():
 
-#import get_mug_transform
-T_cam_mug = get_mug_from_april() #todo once complete
-#T_cam_mug = get_mug_transform() todo
-T_cam_robot = get_transform_camera_robot()
+    zed = ZedCamera()
+    camera_intrinsic = zed.camera_intrinsic
+
+    try:
+        cv_image = zed.image
+
+        # Get Transformation
+        t_cam_robot = get_transform_camera_robot(cv_image, camera_intrinsic)
+        # if t_cam_robot is None:
+        #     return
+        t_cam_tag = None
+        t_robot_cube , t_cam_tag, tag_id = get_transform_cube(cv_image, camera_intrinsic, np.linalg.inv(t_cam_robot))
+        print(t_cam_tag)
+        #import get_mug_transform
+        T_cam_mug = get_mug_from_april(t_cam_tag, tag_id) #todo once complete
+        #T_cam_mug = get_mug_transform() todo
 
 
-# Initialize Lite6 Robot
-arm = XArmAPI(robot_ip)
-arm.connect()
-arm.motion_enable(enable=True)
+        # Initialize Lite6 Robot
+        arm = XArmAPI(robot_ip)
+        arm.connect()
+        arm.motion_enable(enable=True)
 
-arm.set_mode(0)
-arm.set_state(0)
-arm.move_gohome(wait=True)
+        arm.set_mode(0)
+        arm.set_state(0)
+        arm.move_gohome(wait=True)
 
-grasp_poses = []
-usr_input = input("Press c to capture a pose, d to delete the last one, and q to quit")
-while usr_input != 'q':
-    if usr_input == 'c':
-        #T_robot_pose
-        grasp_pose = arm.get_position()
-        #T_pose_robot
-        grasp_pose = np.linalg.inv(grasp_pose)
-        #T_pose_cam
-        grasp_pose = np.linalg.inv(T_cam_robot) @ grasp_pose
-        #T_cam_pose
-        grasp_pose = np.linalg.inv(grasp_pose)
-        grasp_poses.append(grasp_pose)
-    if usr_input == 'd':
-        grasp_poses.pop()
-    usr_input = input("Press c to capture a pose, d to delete the last one, and q to quit")
+        grasp_poses = []
+        usr_input = input("Press c to capture a pose, d to delete the last one, and q to quit")
+        while usr_input != 'q':
+            if usr_input == 'c':
+                #T_robot_pose
+                grasp_xyz_rot = arm.get_position()
+                grasp_xyz = grasp_xyz_rot[1][:3]
+                grasp_rpy = grasp_xyz_rot[1][3:]
+                r = R.from_euler('xyz', grasp_rpy, degrees=True)
+                grasp_pose = np.eye(4)
+                grasp_pose[:3, :3] = r.as_matrix()
 
-with open('./poses.pkl', 'wb') as fp:
-    pickle.dump(grasp_poses, fp)
-arm.disconnect()
+                grasp_pose[:3, 3] = grasp_xyz
+                print(grasp_pose)
+                #T_pose_robot
+                grasp_pose = np.linalg.inv(grasp_pose)
+                #T_pose_cam
+                grasp_pose = np.linalg.inv(t_cam_robot) @ grasp_pose
+                #T_cam_pose
+                grasp_pose = np.linalg.inv(grasp_pose)
+                grasp_poses.append(grasp_pose)
+            if usr_input == 'd':
+                grasp_poses.pop()
+            usr_input = input("Press c to capture a pose, d to delete the last one, and q to quit")
+
+        with open('./poses.pkl', 'wb') as fp:
+            pickle.dump(grasp_poses, fp)
+        arm.disconnect()
+
+    finally:
+        # Close Lite6 Robot
+        time.sleep(0.5)
+
+        # Close ZED Camera
+        zed.close()
+if __name__ == "__main__":
+    main()
